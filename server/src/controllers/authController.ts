7a1aea3b274c74119a11921f81e641765ea68c21
Max Le <minhphuongsony@gmail.com>
2022-10-03 22:28:35 +1100

    has password and create jwt

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { User } from "../../models/User";
+import { User } from "../models/User";
 import { CustomApiError } from "../errors/customApiError";
 
 export const register = async (req: Request, res: Response) => {
@@ -18,7 +18,9 @@ export const register = async (req: Request, res: Response) => {
 
   const user = await User.create(req.body);
 
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJwt();
+
+  res.status(StatusCodes.CREATED).json({ token });
 };
 
 export const login = async (req: Request, res: Response) => {
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,7 @@
-import mongoose, { Schema, model } from "mongoose";
+import { Schema, model, Model } from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export interface User {
   name: string;
@@ -7,6 +9,10 @@ export interface User {
   password: string;
   lastName: string;
   location: string;
+}
+
+export interface UserMethods {
+  createJwt(): string;
 }
 
 const schema = new Schema<User>({
@@ -30,6 +36,7 @@ const schema = new Schema<User>({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -45,4 +52,21 @@ const schema = new Schema<User>({
   },
 });
 
-export const User = model("User", schema);
+schema.pre<User>("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+schema.method("createJwt", function createJwt() {
+  return jwt.sign(
+    { userId: this._id },
+    process.env.JWT_SECRET ?? "notASecret",
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+});
+
+type UserModel = Model<User, {}, UserMethods>;
+
+export const User = model<User, UserModel>("User", schema);
